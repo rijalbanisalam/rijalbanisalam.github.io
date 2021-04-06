abf9471e3268b5c9a9bc03a812f62cb16eb98d5f
rizalalatas <rizalalatas@gmail.com>
2021-04-06 16:43:42 +0700

    rizal

---
+++ style.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(window).bind("load",function(){$("#amtreadramadhan").animate({top:"25%"},1e3),$("a.closepopup").click(function(){return $(this).parent().fadeOut(1e3),!1})});
+//]]>
+</script>
